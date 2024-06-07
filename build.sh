86a33abec79877a42692972ee728bc8cb90f28a0
chelizichen <1347290221@qq.com>
2024-06-07 09:23:30 +0800

    feat:打包问题

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 readonly ServerName="FileSystemServer"
 
 rm -r $ServerName.tar.gz
-
+#rm -r target
 # 如果有问题可以Idea编译，再打包
-mvn compile
-mvn deploy
+#mvn compile
+#mvn deploy
 
 tar -cvf $ServerName.tar.gz ./target
